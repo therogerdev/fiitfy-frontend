b757556fd21f5d80fe35de69d82a6630fdc4f263
Rogerio <91988248+therogerdev@users.noreply.github.com>
2024-10-16 13:52:17 +0700

    configure axios (#26)
    Co-authored-by: Rogerio <91988248+rogerio-js@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { Button } from './components/ui/button';
 import {
   QueryClient,
-  QueryClientProvider,
+  QueryClientProvider
 } from '@tanstack/react-query';
 import './App.css';
+import ProgramList from './components/Programs/ProgramList';
+import { Button } from './components/ui/button';
 
 // Create a client
 const queryClient = new QueryClient();
 
 function App() {
+
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className='App'>
+        <ProgramList />
         <Button>Click me</Button>
       </div>
     </QueryClientProvider>
---
+++ src/axios.config.ts
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.BASE_API_URL;
