fe7f2531e1a11e0099a9e46aa36669b8fc40f016
Rogerio <91988248+therogerdev@users.noreply.github.com>
2024-10-16 13:08:53 +0700

    setup tsconfig (#24)
    Co-authored-by: Rogerio <91988248+rogerio-js@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'), // Main alias for src
+      '@components': path.resolve(__dirname, './src/components'), // Alias for components
+      '@hooks': path.resolve(__dirname, './src/hooks'), // Alias for hooks
     },
   },
-})
+});
