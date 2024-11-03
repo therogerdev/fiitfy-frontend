31643d9febb48d0e6140dbf0a3ce0499fbb2926e
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-11-03 12:43:57 +0700

    basic class by id component

--- src/components/Class/ClassDetail.tsx
+++ src/components/Class/ClassDetail.tsx
@@ -1,5 +1,48 @@
+import { apiClient } from '@/config/axios.config';
+import { EndpointType } from '@/types/api';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router';
+import { Button } from '../ui/button';
+
+const fetchClassDetail = async (id: string) => {
+  const response = await apiClient.get(`${EndpointType.Class}/${id}`);
+
+  return response.data;
+};
+
 const ClassDetail = () => {
-  return <div>ClassDetail</div>;
+  const { id } = useParams();
+  const {
+    data: classDetail,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['class', id],
+    queryFn: () => fetchClassDetail(id as string),
+    enabled: !!id,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+
+
+
+  return (
+    <div className='p-6'>
+      <h1 className='text-2xl font-bold'>{classDetail?.data.name}</h1>
+      <p>{classDetail?.data.id}</p>
+
+      <p>{classDetail?.data.description}</p>
+      <Button>Enroll to this Class</Button>
+
+    </div>
+  );
 };
 
 export default ClassDetail;
