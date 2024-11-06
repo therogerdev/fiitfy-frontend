8a9a46279732e7877d155d65623f5a1c2605479c
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-11-06 12:50:40 +0700

    create page header component

--- src/pages/AthletesAdminPage.tsx
+++ src/pages/AthletesAdminPage.tsx
@@ -4,6 +4,7 @@ import AthleteTable from '@/components/Athletes/AthleteTable';
 import ContentLayout from '@/components/layouts/content-layout';
 import { PageHeader } from '@/components/layouts/page-header';
 import BreadcrumbComponent from '@/components/ui/BreadcrumbsComponent';
+import { Button } from '@/components/ui/button';
 import { appLink } from '@/config/links';
 import { Outlet } from 'react-router';
 
@@ -11,7 +12,12 @@ const AthletesAdminPage = () => {
   return (
     <ContentLayout className=''>
       <BreadcrumbComponent links={[appLink.athletes]} />
-      <PageHeader />
+      <PageHeader title="Athletes" actions={
+        <>
+        <Button>Action 1</Button>
+        <Button variant={"outline"}>Action 2</Button>
+        </>
+      } />
       <AthleteActions />
       <AthleteSearchBar />
       <AthleteTable />
