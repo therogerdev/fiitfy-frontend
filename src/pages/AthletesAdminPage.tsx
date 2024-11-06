5329e612d7b5069fad5164ec5494a7be4728e3a7
Rogerio <91988248+therogerdev@users.noreply.github.com>
2024-11-06 13:35:11 +0700

    Create AthleteActions Component (#52)
    Co-authored-by: Rogerio <91988248+rogerio-js@users.noreply.github.com>

--- src/pages/AthletesAdminPage.tsx
+++ src/pages/AthletesAdminPage.tsx
@@ -4,21 +4,25 @@ import AthleteTable from '@/components/Athletes/AthleteTable';
 import ContentLayout from '@/components/layouts/content-layout';
 import { PageHeader } from '@/components/layouts/page-header';
 import BreadcrumbComponent from '@/components/ui/BreadcrumbsComponent';
-import { Button } from '@/components/ui/button';
 import { appLink } from '@/config/links';
 import { Outlet } from 'react-router';
+
 
 const AthletesAdminPage = () => {
   return (
     <ContentLayout className=''>
       <BreadcrumbComponent links={[appLink.athletes]} />
-      <PageHeader title="Athletes" actions={
-        <>
-        <Button>Action 1</Button>
-        <Button variant={"outline"}>Action 2</Button>
-        </>
-      } />
-      <AthleteActions />
+      <PageHeader title='Athletes' actions={
+        <AthleteActions
+        onFilter={() => console.log("first")}
+        onAdd={() => console.log("first")}
+        onPageChange={() => console.log("first")}
+        totalAthletes={300}
+        setRowsPerPage={() => {}}
+        currentPage={1}
+        rowsPerPage={10}
+
+      />} />
       <AthleteSearchBar />
       <AthleteTable />
       <Outlet />
