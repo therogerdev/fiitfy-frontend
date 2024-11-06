b3c112f1aeafe0238887669ff515d744568ba0c7
Rogerio <91988248+therogerdev@users.noreply.github.com>
2024-11-06 12:51:29 +0700

    create page header component (#51)
    Co-authored-by: Rogerio <91988248+rogerio-js@users.noreply.github.com>

--- src/components/layouts/page-header.tsx
+++ src/components/layouts/page-header.tsx
@@ -1,12 +1,14 @@
 export const PageHeader = ({
-    actions,
-  }: {
-    actions?: React.ReactNode;
-  }) => {
-    return (
-      <div className='flex items-center justify-between space-y-2'>
-        <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
-        <div className='flex items-center space-x-2'>{actions}</div>
-      </div>
-    );
-  };
+  actions,
+  title,
+}: {
+  actions?: React.ReactNode;
+  title: string;
+}) => {
+  return (
+    <div className='flex items-center justify-between space-y-2'>
+      <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
+      <div className='flex items-center space-x-2'>{actions}</div>
+    </div>
+  );
+};
