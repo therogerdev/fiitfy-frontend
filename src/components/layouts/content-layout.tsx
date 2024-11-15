b5d5adb46f0f4cc1b73364e525858c47329eae88
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-11-15 17:40:42 +0700

    content layout refactor

--- src/components/layouts/content-layout.tsx
+++ src/components/layouts/content-layout.tsx
@@ -1,19 +1,28 @@
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 const ContentLayout = ({
+  header,
   children,
   className,
+  breadcrumbs,
 }: {
+  header?: React.ReactNode;
+  breadcrumbs?: React.ReactNode;
   children: React.ReactNode;
   className?: string;
 }) => {
   return (
     <section
-      className={cn(
-        className,
-        '  p-4 sm:px-10 sm:py-0 md:gap-8 lg:py-6'
-      )}
+      className={cn(className, "p-4 sm:px-10 sm:py-0 md:gap-8 lg:py-6 ")}
     >
+      {breadcrumbs && <>{breadcrumbs}</>}
+      {header && (
+        <div className="flex justify-end">
+          <div className="flex justify-end w-full max-w-6xl px-8 py-3 mxl-auto">
+            {header}
+          </div>
+        </div>
+      )}
       {children}
     </section>
   );
