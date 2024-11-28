2a40f8f38190ee9842f7dc2c0e5b40f1dc8fe729
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-11-28 10:40:08 +0700

    fetchClassDetail service

--- src/services/classes.ts
+++ src/services/classes.ts
@@ -1,12 +1,21 @@
 import { apiClient } from "@/config/axios.config";
 import { ClassResponse } from "@/types";
+import { EndpointType } from "@/types/api";
 
 export const fetchClassesBySelectedDate = async (
   startDate: string,
   endDate: string
 ): Promise<ClassResponse> => {
   const response = await apiClient.get(
-    `/class/list?startTime=${startDate}&endTime=${endDate}`
+    `${EndpointType.Class}/list?startTime=${startDate}&endTime=${endDate}`
   );
+  return response.data;
+};
+
+
+
+export const fetchClassDetail = async (id: string) => {
+  const response = await apiClient.get(`${EndpointType.Class}/${id}`);
+
   return response.data;
 };
